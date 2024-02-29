acef15b01aeaabaf083399d70febba093d490b01
mahade <mahade.adib45@gmail.com>
2024-02-29 20:51:45 +0600

    added more functions

--- js/phone.js
+++ js/phone.js
@@ -36,8 +36,8 @@ const displayPhones = (phones, isShowAll) =>{
             <div class="card-body">
                 <h2 class="card-title">${phone.phone_name}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                <div class="card-actions justify-center">
+                    <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary px-16">Show Details</button>
                 </div>
             </div>
         `
@@ -49,6 +49,12 @@ const displayPhones = (phones, isShowAll) =>{
     toggleLoadingSpinner(false);
 }
 
+//
+const handleShowDetail = async(id) =>{
+    //load single phone data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+}
 
 // handle search button
 const handleSearch = (isShowAll) =>{
